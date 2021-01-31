618e188c6511f22a958fa22c705c130ff9124631
yoshin04 <taigaby0411@gmail.com>
2021-01-31 10:42:22 +0900

    node.js課題2完成

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,25 +1,22 @@
 const { validationResult } = require('express-validator');
 
+let errorMessageParams = ``;
+
 module.exports = {
   new: (req, res) => {
     res.render('users/new', {
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
-      errorMessages: errorMessages,
-      errorMessages: '',
+      errorMessages: errorMessageParams,
     });
   },
   create: (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       let errorMessages = errors.array();
-      return res.render('users/new', {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        errorMessages: errorMessages,
-      });
+      errorMessageParams = errorMessages;
+      return res.redirect('/users/new');
     }
     return res.render('users/index', {
       name: req.body.name,
